628a3dae07d6e3e954bd09e30c818d97bdcc0e49
Mostafa Haque <93haque@gmail.com>
2018-11-01 12:03:32 -0400

    Light follow mouse script

---
+++ Assets/Scripts/Choices/ChoicePoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoicePoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.CompareTag("MimLight"))
+		{
+			Debug.Log("Light is in zone;");
+
+			if (other.GetComponent<LightController>().lerpProgress == 1)
+			{
+				Debug.Log("Choice is made");
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/LightFollowMouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollowMouse : MonoBehaviour
+{
+	private Vector3 prevMosuePos;
+	public float horizontalSpeed;
+	public float verticalSpeed;
+
+	// Use this for initialization
+	void Start () {
+		prevMosuePos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//this.transform.position = Input.mousePosition;
+
+		//Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		float h = horizontalSpeed * Input.GetAxisRaw("Mouse X");
+		float v = verticalSpeed * Input.GetAxisRaw("Mouse Y");
+		
+		transform.position = transform.position + new Vector3(h,v,0);
+		
+		/*transform.position += Camera.main.ScreenToWorldPoint(Input.mousePosition) - prevMosuePos;
+		prevMosuePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);*/
+	}
+}
