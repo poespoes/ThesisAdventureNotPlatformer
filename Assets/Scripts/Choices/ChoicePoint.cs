4617f273983ef04d9297c180f794a2a1bfb350a5
Mostafa Haque <93haque@gmail.com>
2018-11-01 15:03:39 -0400

    Mim light collision fixed

--- Assets/Scripts/Choices/ChoicePoint.cs
+++ Assets/Scripts/Choices/ChoicePoint.cs
@@ -9,6 +9,8 @@ public class ChoicePoint : MonoBehaviour {
 	public string boolName;
 	public bool choiceMade;
 
+	private bool lightEntered;
+	private GameObject mimLight;
 
 	// Use this for initialization
 	void Start ()
@@ -18,12 +20,26 @@ public class ChoicePoint : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (lightEntered == true)
+		{
+			Debug.Log("Light is in zone");
+			if (mimLight.GetComponent<LightController>().readyToDecide==true)
+			{
+				choiceMade = true;
+			}
+			
+			if (choiceMade == true)
+			{
+				Debug.Log("The choice has been made");
+				anim.SetBool(boolName,true);
+				//GameObject.Find("GameManager").GetComponent<GameManager>().isInteractive = false;
+			}
+		}
 	}
 
 	private void OnTriggerStay2D(Collider2D other)
 	{
-		if (other.CompareTag("MimLight"))
+		/*if (other.CompareTag("MimLight"))
 		{
 			Debug.Log("Light is in zone;");
 
@@ -38,6 +54,24 @@ public class ChoicePoint : MonoBehaviour {
 			Debug.Log("The choice has been made");
 			anim.SetBool(boolName,true);
 			//GameObject.Find("GameManager").GetComponent<GameManager>().isInteractive = false;
+		}*/
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("MimLight"))
+		{
+			lightEntered = true;
+			mimLight = other.gameObject;
+		}
+	}
+	
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("MimLight"))
+		{
+			lightEntered = false;
+			
 		}
 	}
 }
